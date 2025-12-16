aa8985f2e561a90350789ae26fa19e22f1c4c27a
Akash <dalbhanjanakash1@gmail.com>
2025-12-16 23:46:03 +0530

    attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { TodoList } from "./TodoList";
 
 const App = () => {
+  const todosItems = [
+    {
+      id :1,
+      text : "Learn React",
+      isCompleted : false
+    },
+    {
+      id :2,
+      text : "Build a React app",
+      isCompleted : false
+    },
+    {
+      id :3,
+      text : "Deploy the React app",
+      isCompleted : false
+    }
+  ]
+  const[todos,setTodos] = useState(todosItems);
+  function handleComplete(id) {
+  setTodos((prevTodos) => {
+    const updatedTodos = [...prevTodos];
+    const index = updatedTodos.findIndex(todo => todo.id === id);
+
+    if (index !== -1) {
+      updatedTodos[index] = {
+        ...updatedTodos[index],
+        isCompleted: true
+      };
+    }
+
+    return updatedTodos;
+  });
+}
+// console.log(todos);
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete}/>
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function TodoList({todos,handleComplete}){
+    const completeButton = <button onClick={handleComplete}>Complete</button>
+    return(
+        <>
+        <h2>Child Component</h2>
+        <ul>
+  {todos.map((ele) => (
+    <li key={ele.id}>
+      {ele.text}
+      {!ele.isCompleted && (
+        <button onClick={() => handleComplete(ele.id)}>
+          Complete
+        </button>
+      )}
+    </li>
+  ))}
+</ul>
+
+        </>
+    )
+}
