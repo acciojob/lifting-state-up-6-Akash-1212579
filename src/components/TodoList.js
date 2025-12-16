66acf9b0aba8ea8dae8c689e2dcea31baa0bea64
Akash <dalbhanjanakash1@gmail.com>
2025-12-17 00:17:40 +0530

    attempt 5

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,16 @@
-import React from "react";
-
 export function TodoList({ todos, handleComplete }) {
   return (
-    <>
-      <h2>Child Component</h2>
-      <ul>
-        {todos.map((ele) => (
-          <li key={ele.id}>
-            {ele.text}
-            {!ele.isCompleted && (
-              <button onClick={() => handleComplete(ele.id)}>Complete</button>
-            )}
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {todos.map((ele) => (
+        <li key={ele.id}>
+          {ele.text}
+          {!ele.isCompleted && (
+            <button onClick={() => handleComplete(ele.id)}>
+              Complete
+            </button>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 }
